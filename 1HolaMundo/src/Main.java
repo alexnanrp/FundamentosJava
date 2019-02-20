33ab0f979276df9e3727260bbb49a7fae09fe8df
alexnan <alexnanrp@gmail.com>
2019-02-19 20:06:00 -0600

    cambio en 1HolaMundo variables

--- 1HolaMundo/src/Main.java
+++ 1HolaMundo/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo1");
-        System.out.println("Adios");
+    	String saludos = "Hola Mundo, adios.";
+    	var desp = "despedirse";
+        
+        //Pruebas de impresion con variable saludos
+        System.out.println(saludos);
+        System.out.println(saludos);
+        System.out.println(saludos);
+        
+        //Pruebas de impresion con variable despedirse
+        System.out.println();
+        System.out.println(desp);
     }
 }
