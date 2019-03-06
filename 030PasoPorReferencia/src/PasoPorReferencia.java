36a89584a71b1fd8ef915bd93e3b4782b40e358b
alexnan <alexnanrp@gmail.com>
2019-03-05 21:20:52 -0600

    Se agrego el modulo 030PasoPorValor

---
+++ 030PasoPorReferencia/src/PasoPorReferencia.java
@@ -0,0 +1,17 @@
+public class PasoPorReferencia{
+    public static void main(String[] args){
+        Persona p = new Persona();
+        p.cambiarNombre("Juan");
+        imprimirNombre(p);
+        modificarPersona(p);
+        imprimirNombre(p);
+    }
+
+    public static void imprimirNombre(Persona p1){
+    	System.out.println("Valor recibido: " + p1.obtenerNombre());
+    }
+
+    public static void modificarPersona(Persona arg){
+    	arg.cambiarNombre("Carlos");
+    }
+}
